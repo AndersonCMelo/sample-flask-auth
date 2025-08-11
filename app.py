84d84f54d8a07db092e3bb9a713f590030c2fd1c
Anderson Melo <anderson.melo.ext@spms.min-saude.pt>
2025-08-11 23:49:44 +0100

    Implement user roles and password hash

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud"
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -26,7 +27,7 @@ def login():
         # Login
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print('current_user.is_authenticated', current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
@@ -46,7 +47,8 @@ def create_user():
     password = data.get('password')
 
     if username and password:
-        user = User(username=username, password=password)
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
@@ -69,8 +71,12 @@ def update_user(user_id):
     user = User.query.get(user_id)
     data = request.json
     
+    if user_id != current_user.id and current_user.role == 'user':
+        return jsonify({"message": "Operação não permitida"}), 403
+
     if user and data.get('password'):
-        user.password = data.get('password')
+        hashed_password = bcrypt.hashpw(str.encode(data.get('password')), bcrypt.gensalt())
+        user.password = hashed_password
         db.session.commit()
 
         return jsonify({"message": f"Usuário {user_id} atualizado com sucesso"})
@@ -81,6 +87,9 @@ def update_user(user_id):
 @login_required
 def delete_user(user_id):
     user = User.query.get(user_id)
+    
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
     
     if user_id == current_user.id:
         return jsonify({"message": "Deleção não permitida"}), 403
